edb78770352a5ce00b41b2f602fb1f54d2fae523
shieldadchr <shielda.diasavitri@gmail.com>
2023-03-19 21:42:49 +0700

    menambahkan class dataGeneric

--- tpmod5_1302210044/Program.cs
+++ tpmod5_1302210044/Program.cs
@@ -8,10 +8,22 @@ internal class Program
             Console.WriteLine("Hallo user " + inputuser);
         }
     }
-    
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
     private static void Main(string[] args)
     {
         HaloGeneric.sapaUser<string>("Shielda");
-        
+        DataGeneric<string> nim = new DataGeneric<string>("1302210044");
+        nim.PrintData();
     }
 }
