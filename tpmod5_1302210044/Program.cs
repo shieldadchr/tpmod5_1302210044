62724a7cd4e173c1a673f27b927671f21b08259c
shieldadchr <shielda.diasavitri@gmail.com>
2023-03-19 21:40:09 +0700

    menambahkan class halloGeneric dengan method sapaUsers

--- tpmod5_1302210044/Program.cs
+++ tpmod5_1302210044/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T inputuser)
+        {
+            Console.WriteLine("Hallo user " + inputuser);
+        }
+    }
+    
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("Shielda");
+        
+    }
+}
